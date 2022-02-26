3e49970174e69996594596dbae9da1962b42a006
Josef Handl <handljosef@gmail.com>
2022-02-26 21:47:28 +0100

    added changing default shell

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ ! -e "/bin/zsh" ]; then
+	echo "Install zsh and start this script again"
+	exit 1
+fi
+
+chsh -s /bin/zsh
+
+sleep 1
+
+if [ "/bin/zsh" != $(cat /etc/passwd | grep $USER | cut -d":" -f7) ]; then
+	echo "Changing the default shell failed. You have to change it manually and start this script again"
+	exit 2
+fi
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
