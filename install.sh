029c0b0995b31c3709dbcaa346b7b562e9693e31
Josef Handl <handljosef@gmail.com>
2022-02-26 21:17:02 +0000

    fixed script

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+change_shell () {
+	echo "Changing the default shell failed. You have to change it manually and start this script again"
+	exit 2
+}
+
 if [ ! -e "/bin/zsh" ]; then
 	echo "Install zsh and start this script again"
 	exit 1
 fi
 
-chsh -s /bin/zsh
+chsh -s /bin/zsh || change_shell
 
 sleep 1
 
 if [ "/bin/zsh" != $(cat /etc/passwd | grep $USER | cut -d":" -f7) ]; then
-	echo "Changing the default shell failed. You have to change it manually and start this script again"
-	exit 2
+	change_shell
 fi
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
@@ -24,3 +28,6 @@ ABS_PATH="$(pwd)/$(dirname "$0")"
 
 ln -s "$ABS_PATH/.vimrc" ~/.vimrc
 ln -s "$ABS_PATH/.p10k.zsh" ~/.p10k.zsh
+
+rm ~/.zshrc
+ln -s "$ABS_PATH/.zshrc" ~/.zshrc
