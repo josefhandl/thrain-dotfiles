fe978140a16f3ef8acfc9d4304348849f32f069c
Josef Handl <handljosef@gmail.com>
2024-10-14 10:23:52 +0200

    ufw: allow samba for whole kvm-qemu VMs

--- ufw.sh
+++ ufw.sh
@@ -11,9 +11,9 @@ sudo ufw default allow outgoing
 sudo ufw allow from 0.0.0.0/0 to any port 22 proto tcp
 sudo ufw allow from ::/0 to any port 22 proto tcp
 
-# Allow samba for Thror
-sudo ufw allow from 192.168.122.43/32 to any port 139 proto tcp
-sudo ufw allow from 192.168.122.43/32 to any port 445 proto tcp
+# Allow samba for KVM-Qemu VMs
+sudo ufw allow from 192.168.122.0/24 to any port 139 proto tcp
+sudo ufw allow from 192.168.122.0/24 to any port 445 proto tcp
 
 sudo ufw deny 22
 sudo ufw deny 139
